6be2cac4f20eddb4f07720c3872d9d05bde68a72
Baliram Gupta <balramgupta7054@gmail.com>
2024-07-19 08:11:43 +0530

    B_Number_Circle

---
+++ B_Number_Circle.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    long long  t;
+    cin>>t;
+     int arr[t];
+    for(int i=0;i<t;i++){
+        cin>>arr[i];
+    }
+    sort(arr,arr+t);
+    if(arr[t-1]>=arr[t-2]+arr[t-3])
+        cout<<"NO"<<endl;
+    else{
+        cout<<"YES"<<endl;
+        cout<<arr[t-1]<<" ";
+        for(int i=t-3;i>=0;i--){
+            cout<<arr[i]<<" ";
+        }
+        cout<<arr[t-2]<<"\n";
+    }
+    return 0;
+}
